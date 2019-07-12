c78bc6692588b814af707a52b6e9c6ef6156d819
cdra0624 <t.adaniya@adawarp.com>
2019-07-12 14:05:52 +0900

    chore: add karate image

--- src/pages/top.js
+++ src/pages/top.js
@@ -1,14 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import karateImg from "../assets/images/karate.jpg"
 
 const TopPage = () => (
-  <div>
+  <div className="container">
     <h1>Scheme Dojo</h1>
-    <ul>
-      <li>
-        <Link to="/belt-guide">Scheme Dojo Belt Guide</Link>
-      </li>
-    </ul>
+    <div className="content">
+      <ul>
+        <li>
+          <Link to="/belt-guide">Scheme Dojo Belt Guide</Link>
+        </li>
+        <li>
+          <Link to="/zigen-belt">Zigen's belt status</Link>
+        </li>
+        <li>
+          <Link to="/ada-belt">Adaniya's belt status</Link>
+        </li>
+        <li>
+          <Link to="/rah-belt">Rahmat's belt status</Link>
+        </li>
+        <li>
+          <Link to="/mamadou-belt">Mamadou's belt status</Link>
+        </li>
+      </ul>
+      <img src={karateImg}/>
+    </div>
+    <style jsx>{`
+      div.container {
+        margin: 0 2rem;
+      }
+      div.content {
+        display: flex;
+      }
+      li {
+        margin: 1rem 0;
+      }
+      img {
+        margin-top: 1.5rem;
+        margin-left: 5rem;
+        width: 10rem;
+        height: 10rem;
+      }
+    `}</style>
   </div>
 );
 
