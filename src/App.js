5d0fada4e924801c9e3aa65ebbf45f876c384c74
cdra0624 <t.adaniya@adawarp.com>
2019-07-12 13:48:16 +0900

    refactoring: remove css and use styled component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
 
 function App() {
   return (
@@ -19,6 +18,29 @@ function App() {
           Learn React
         </a>
       </header>
+      <style jsx>{`
+        .App {
+          text-align: center;
+        }
+        .App-header {
+          background-color: #282c34;
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          font-size: calc(10px + 2vmin);
+          color: white;
+        }
+        .App-logo {
+          animation: App-logo-spin infinite 20s linear;
+          height: 40vmin;
+          pointer-events: none;
+        }
+        .App-link {
+          color: #61dafb;
+        }
+      `}</style>
     </div>
   );
 }
